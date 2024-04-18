3bdef6bc67b24c0962e911f3ab51e0aec4d3614d
Pandjie <m.pandjie2007@gmail.com>
2024-04-18 12:28:19 +0700

    test

---
+++ H32/part 1/input.php
@@ -0,0 +1,30 @@
+<div class="container">
+<h1 class="tengah">CRUD OOP PHP</h1>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<h3 class="tengah1">Tambah Data User</h3>
+ 
+<form action="proses.php?aksi=tambah" method="post">
+	
+		<div class="all">
+<table>
+	<tr>
+		<td>Nama :</td>
+		<td><input type="text" name="nama"></td>
+	</tr>
+	<tr>
+		<td>Alamat :</td>
+		<td><input type="text" name="alamat"></td>
+	</tr>
+	<tr>
+		<td>Usia :</td>
+		<td> <input type="text" name="usia"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td> <input type="submit" value="Simpan" id="button" ></td>
+	</tr>
+</table>
+</div>
+</div>
+</form>
